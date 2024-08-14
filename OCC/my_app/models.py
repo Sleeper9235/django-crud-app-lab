7d127acd616dc999b9b24ecf69e496e61b777fa4
Sleeper9235 <kylejyoumans@gmail.com>
2024-08-14 15:25:32 -0500

    updated models

--- OCC/my_app/models.py
+++ OCC/my_app/models.py
@@ -4,7 +4,9 @@ from django.db import models
 class Group(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=250)
-    image = models.FilePathField(path = "my_app/static/images")
+    image = models.ImageField(upload_to='my_app/static/images', height_field='height', width_field='width', null=True, blank=True)
+    height = models.PositiveIntegerField(null=True, blank=True)
+    width = models.PositiveIntegerField(null=True, blank=True)
 
     def __str__ (self):
         return self.name
--- OCC/my_app/views.py
+++ OCC/my_app/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from .models import Group
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    groups = Group.objects.all()
+    return render(request, 'home.html', {'groups': groups})
 
 def about(request): 
     return render(request, 'about.html')
